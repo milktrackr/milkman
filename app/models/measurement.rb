5989da75ead28449fd093ee39d3046b861bad6be
Daniel Fenjves <fenjves.daniel@gmail.com>
2014-03-31 18:21:14 -0400

    all set up and ready to go

---
+++ app/models/measurement.rb
@@ -0,0 +1,10 @@
+class Measurement < ActiveRecord::Base
+
+
+  # The density varies from 1.032kg/L (whole milk) to 1.035kg/L (skim milk). 
+  # This can go as low as 1.026kg/L under certain conditions, but a low density could also indicate added water.
+
+
+
+
+end
---
+++ db/migrate/20140331220719_create_measurements.rb
@@ -0,0 +1,12 @@
+class CreateMeasurements < ActiveRecord::Migration
+  def change
+    create_table :measurements do |t|
+    	t.integer   :raw
+      t.datetime  :read_time
+      t.float     :mass_value
+      t.string    :mass_uom
+      
+      t.timestamps
+    end
+  end
+end
