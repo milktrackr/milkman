52cb0f6f9b30f5121c59f911968591379a52180a
Corbin Page <corbin.page@gmail.com>
2014-04-01 13:17:49 -0400

    worked on conversions

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,10 +1,32 @@
 class Measurement < ActiveRecord::Base
 
+GRAMS_SLOPE_ROOM_TEMP = 2.395698737
+GRAMS_INTERCEPT_ROOM_TEMP = -465.6012396
 
-  # The density varies from 1.032kg/L (whole milk) to 1.035kg/L (skim milk). 
-  # This can go as low as 1.026kg/L under certain conditions, but a low density could also indicate added water.
+  def convert_raw_to_grams
+    g_uom = 'g'
+    g_value = ((self.raw * GRAMS_SLOPE_ROOM_TEMP) + GRAMS_INTERCEPT_ROOM_TEMP).round
 
+    # raw 0-1024
+    # kg ~0-6
+    # Need to control for noise
 
+    [g_value, g_uom]
+  end
 
+  def convert_grams_to_liters
+    # Milk density varies from 1.027kg/L to 1.033kg/L. 
+    # We'll use 1.030kg/L for simplicity
+    milk_density = 1030.0 # g/L
+
+    liter_uom = 'L'
+    liter_value = (convert_raw_to_grams[0] / milk_density).round
+    [liter_value, liter_uom]
+  end
+
+  def percentage_left
+  # Might need a container to build this out
+
+  end
 
 end
