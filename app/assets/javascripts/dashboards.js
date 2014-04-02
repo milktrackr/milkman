ddc5f1359dd248e10acfe8092a47cdf8f94ac50e
Daniel Fenjves <fenjves.daniel@gmail.com>
2014-04-02 09:58:33 -0400

    line chart working on dashboard finally

--- app/assets/javascripts/dashboards.js
+++ app/assets/javascripts/dashboards.js
@@ -1,3 +1,52 @@
+console.log("This is a test")
 
-    $('#milk-line').hide();
-console.log("ugh")
+$(document).ready(function(){
+
+$('#milk-line').highcharts({
+            chart: {
+            	backgroundColor: "#f2f2f2",
+	        		borderColor: null,
+              type: 'spline',
+            },
+            title: {
+                text: null
+            },
+            legend: {
+            	enabled: false
+            },
+            credits: {
+      				enabled: false
+  					},
+            xAxis: {
+                type: 'datetime',
+                dateTimeLabelFormats: { // don't display the dummy year
+                    month: '%e. %b',
+                    year: '%b'
+                }
+            },
+            yAxis: {
+                title: {
+                    text: 'Milk (ml)'
+                },
+                max: 2000,
+                min: 0
+            },
+            tooltip: {
+                formatter: function() {
+                        return '<b>'+ this.series.name +'</b><br/>'+
+                        Highcharts.dateFormat('%e. %b', this.x) +': '+ this.y +' ml';
+                }
+            },
+            
+            series: [{
+                name: 'Milk Consumption',
+                // Define the data points. All series have a dummy year
+                // of 1970/71 in order to be compared on the same x axis. Note
+                // that in JavaScript, months start at 0 for January, 1 for February etc.
+                data: gon.all_measurements
+            }]
+        });
+});
+
+
+
