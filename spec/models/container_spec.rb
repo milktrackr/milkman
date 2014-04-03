df74b52bcf7dd23278dc616d5509825a33019449
Corbin Page <corbin.page@gmail.com>
2014-04-03 11:38:18 -0400

    Changes made a fully tested

--- spec/models/container_spec.rb
+++ spec/models/container_spec.rb
@@ -1,14 +1,19 @@
 require 'spec_helper'
 
 describe Container do
-  # it "calculates the percentage left" do
-  #   c = Container.create( original_mass:  800,
-  #                         mass_uom:       'g',
-  #                         creation_time:  Time.now - 80.minutes)
-  #   Measurement.create(raw: 800, read_time: Time.now - 70.minutes)
-  #   expect(c.percentage_left).to eq(100)
-  #   Measurement.create(raw: 700, read_time: Time.now - 60.minutes)
-  #   Measurement.create(raw: 400, read_time: Time.now - 50.minutes)
-  #   expect(c.percentage_left).to eq(50)
-  # end
+  it "calculates the percentage left in a container" do
+    Measurement.log_new_measurement({raw: 800, read_time: Time.now - 80.minutes}).save
+    expect(Container.last.percentage_left).to eq(100)
+    Measurement.log_new_measurement({raw: 600, read_time: Time.now - 70.minutes}).save
+    expect(Container.last.percentage_left).to eq(67)
+    Measurement.log_new_measurement({raw: 400, read_time: Time.now - 60.minutes}).save
+    expect(Container.last.percentage_left).to eq(34)
+    Measurement.log_new_measurement({raw: 401, read_time: Time.now - 50.minutes}).save
+    Measurement.log_new_measurement({raw: 200, read_time: Time.now - 40.minutes}).save
+    expect(Container.last.percentage_left).to eq(1)
+    Measurement.log_new_measurement({raw: 0, read_time: Time.now - 30.minutes}).save
+    expect(Container.last.percentage_left).to eq(0)
+    Measurement.log_new_measurement({raw: 800, read_time: Time.now - 80.minutes}).save
+    expect(Container.last.percentage_left).to eq(100)
+  end
 end
