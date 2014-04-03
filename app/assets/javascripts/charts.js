ee13bfd6772bdf0be0d44a95d47b4e26f046908e
Daniel Fenjves <fenjves.daniel@gmail.com>
2014-04-03 10:03:07 -0400

    got rid of chart

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -103,55 +103,5 @@ $(".measurements.index").ready(function(){
 	    }]
 	
 	});
-
-$('#highchart-line').highcharts({
-            chart: {
-            	backgroundColor: "#f2f2f2",
-	        		borderColor: null,
-              type: 'spline',
-              width: 360,
-	        		height: 180
-            },
-            title: {
-                text: null
-            },
-            legend: {
-            	enabled: false
-            },
-            credits: {
-      				enabled: false
-  					},
-            xAxis: {
-                type: 'datetime',
-                dateTimeLabelFormats: { // don't display the dummy year
-                    month: '%e. %b',
-                    year: '%b',
-                    minute: '%I:%M %p',
-                    hour: '%I:%M %p'
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'Milk (ml)'
-                },
-                max: 2000,
-                min: 0
-            },
-            tooltip: {
-                formatter: function() {
-                        return '<b>'+ this.series.name +'</b><br/>'+
-                        Highcharts.dateFormat('%b. %e, %I:%M %p', this.x) +': '+ this.y +' ml';
-                }
-            },
-            
-            series: [{
-                name: 'Milk Consumption',
-                // Define the data points. All series have a dummy year
-                // of 1970/71 in order to be compared on the same x axis. Note
-                // that in JavaScript, months start at 0 for January, 1 for February etc.
-                data: gon.all_measurements
-            }]
-        });
-
 		
 });
