e45dc8d1e77172524092dffd513d3719e51b2548
ErikPeterson <eriksalgstrom@gmail.com>
2014-04-07 10:26:14 -0400

    Passing controller test

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,13 +1,18 @@
 class DashboardsController < ApplicationController
 
 	def index
+		
+			binding.pry
 		@measurements = Measurement.all
 		@containers = Container.all
 		gon.current_measurement = @measurements.last.mass_value
-    gon.all_measurements = []
-    @measurements.all.each do |measurement|
-      gon.all_measurements<< [measurement.read_time.utc.to_i*1000, measurement.mass_value]
-    end
+    
+    	gon.all_measurements = []
+    
+	    @measurements.all.each do |measurement|
+	      gon.all_measurements << [measurement.read_time.utc.to_i*1000, measurement.mass_value]
+	    end
+	
 	end
 
 end
